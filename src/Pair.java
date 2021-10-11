d0d331f7d0441f65c05cba2349175375ef4a96f3
GhermanCristian <gherman_cristi10@yahoo.com>
2021-10-11 13:57:48 +0300

    Added the Pair class, will be used to store positions in the SymbolTable

---
+++ src/Pair.java
@@ -0,0 +1,17 @@
+public class Pair {
+    private final Integer first;
+    private final Integer second;
+
+    public Integer getFirst() {
+        return this.first;
+    }
+
+    public Integer getSecond() {
+        return this.second;
+    }
+
+    public Pair(Integer first, Integer second) {
+        this.first = first;
+        this.second = second;
+    }
+}
