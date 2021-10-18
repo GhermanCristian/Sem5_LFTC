5bec7240e5d6c643378f3eaa925261719b87b2a6
GhermanCristian <gherman_cristi10@yahoo.com>
2021-10-18 14:14:12 +0300

    Added toString to SymbolTable

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -53,4 +53,13 @@ public class SymbolTable {
             this.table.add(new ArrayList<>());
         }
     }
+
+    @Override
+    public String toString() {
+        StringBuilder representation = new StringBuilder();
+        for (int i = 0; i < this.table.size(); i++) {
+            representation.append(i).append(" - ").append(this.table.get(i)).append("\n");
+        }
+        return representation.toString();
+    }
 }
