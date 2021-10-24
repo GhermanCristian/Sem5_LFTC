05a5762681a327bde71c5fea4f0e63bdc61d4e69
GhermanCristian <gherman_cristi10@yahoo.com>
2021-10-24 23:10:25 +0300

    Can now detect the line number of each token

--- src/MyScanner.java
+++ src/MyScanner.java
@@ -7,7 +7,7 @@ public class MyScanner {
     private final ArrayList<String> OPERATORS = new ArrayList<>(
             List.of("+", "-", "*", "/", "<=", ">=", "==", "<", ">", "%", "=", "cin>>", "cout<<"));
     private final ArrayList<String> SEPARATORS = new ArrayList<>(
-            List.of("{", "}", ":", ";", " ", ",", "[", "]", "(", ")", "\""));
+            List.of("{", "}", ":", ";", " ", ",", "[", "]", "(", ")", "\"", "\n"));
     private final ArrayList<String> RESERVED_WORDS = new ArrayList<>(
             List.of("int", "char", "void", "struct", "while", "if", "else", "main"));
 
@@ -23,45 +23,48 @@ public class MyScanner {
         StringBuilder fileContent = new StringBuilder();
         Scanner scanner = new Scanner(new File(this.filePath));
         while (scanner.hasNextLine()) {
-            fileContent.append(scanner.nextLine());
+            fileContent.append(scanner.nextLine()).append("\n");
         }
-        return fileContent.toString()
-                .replace("\n", "")
-                .replace("\t", "");
+        return fileContent.toString().replace("\t", "");
     }
 
-    private List<String> tokenizeWithCompleteStrings(List<String> tokensIncludingSeparators) {
-        List<String> tokensWithCompleteStrings = new ArrayList<>();
+    private List<Pair<String, Integer>> tokenizeWithCompleteStringsAndLineNumbers(List<String> tokensIncludingSeparators) {
+        List<Pair<String, Integer>> tokensWithCompleteStrings = new ArrayList<>();
         boolean inString = false;
         StringBuilder currentString = new StringBuilder();
+        int lineNumber = 1;
+
         for (String token : tokensIncludingSeparators) {
             if (token.equals("\"")) {
                 if (inString) { // end of string
-                    tokensWithCompleteStrings.add(currentString.toString());
+                    tokensWithCompleteStrings.add(new Pair<>(currentString.toString(), lineNumber));
                     currentString = new StringBuilder();
                 }
                 inString = !inString;
+            }
+            else if (token.equals("\n")) {
+                lineNumber++;
             }
             else {
                 if (inString) { // add the current token to the string
                     currentString.append(token);
                 }
                 else if (!this.SEPARATORS.contains(token)) {
-                    tokensWithCompleteStrings.add(token);
+                    tokensWithCompleteStrings.add(new Pair<>(token, lineNumber));
                 }
             }
         }
         return tokensWithCompleteStrings;
     }
 
-    private List<String> tokenize() {
+    private List<Pair<String, Integer>> tokenize() {
         try {
             String fileContent = this.readFile();
             String separators = this.SEPARATORS.stream().reduce("", (a, b) -> a + b);
             List<String> tokensIncludingSeparators = Collections.list(new StringTokenizer(fileContent, separators, true)).stream()
                     .map(token -> (String) token)
                     .collect(Collectors.toList());
-            return this.tokenizeWithCompleteStrings(tokensIncludingSeparators);
+            return this.tokenizeWithCompleteStringsAndLineNumbers(tokensIncludingSeparators);
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -71,21 +74,21 @@ public class MyScanner {
     }
 
     private void addToSymbolTable() {
-        List<String> tokens = this.tokenize();
+        List<Pair<String, Integer>> tokens = this.tokenize();
         if (tokens == null) {
             return;
         }
-        tokens.forEach(token -> {
-            System.out.print(token);
-            if (this.RESERVED_WORDS.contains(token)) {
+        tokens.forEach(tokenLinePair -> {
+            System.out.print(tokenLinePair.getFirst() + "   " + tokenLinePair.getSecond());
+            if (this.RESERVED_WORDS.contains(tokenLinePair.getFirst())) {
                 System.out.print(" - reserved word");
             }
-            else if (this.OPERATORS.contains(token)) {
+            else if (this.OPERATORS.contains(tokenLinePair.getFirst())) {
                 System.out.print(" - operator");
             }
             else {
                 System.out.print(" - identifier / constant");
-                this.symbolTable.add(token);
+                this.symbolTable.add(tokenLinePair.getFirst());
             }
             System.out.println();
         });
@@ -93,6 +96,6 @@ public class MyScanner {
 
     public void scan() {
         this.addToSymbolTable();
-        System.out.println(this.symbolTable);
+        //System.out.println(this.symbolTable);
     }
 }
