33cbdaa3b149fbe47b5cf62b288e99112d582855
GhermanCristian <gherman_cristi10@yahoo.com>
2021-11-25 21:39:34 +0200

    Moved the code responsible for computing FOLLOW sets to a separate class

---
+++ src/FollowSet.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FollowSet {
+    private Map<String, Set<String>> followSets;
+    private final Grammar grammar;
+    private final FirstSet firstSets;
+
+    private List<Pair<String, Set<List<String>>>> findProductionsWhichContainNonterminal(String nonterminal) {
+        List<Pair<String, Set<List<String>>>> productionsWhichContainNonterminal = new ArrayList<>();
+        this.grammar.getProductions().forEach((productionLHS, productionRHS) -> {
+            Set<List<String>> productionRHSWhichStartFromCurrentNonterminal = productionRHS
+                    .stream()
+                    .filter(possibleProductionRHS -> possibleProductionRHS.contains(nonterminal))
+                    .collect(Collectors.toSet());
+            if (! productionRHSWhichStartFromCurrentNonterminal.isEmpty()) {
+                productionsWhichContainNonterminal.add(new Pair<>(productionLHS.get(0), productionRHSWhichStartFromCurrentNonterminal));
+            }
+        });
+        return productionsWhichContainNonterminal;
+    }
+
+    private void processPossibleProductionRHS(Map<String, Set<String>> temporaryFollowSets, String nonterminal, List<String> possibleProductionRHS, String LHSNonterminal) {
+        List<String> elementsAfterNonterminalFirstOccurrence = possibleProductionRHS.subList(possibleProductionRHS.indexOf(nonterminal) + 1, possibleProductionRHS.size());
+        if (elementsAfterNonterminalFirstOccurrence.size() == 0) {
+            elementsAfterNonterminalFirstOccurrence.add(Constants.EPSILON);
+        }
+
+        Set<String> FIRST = this.firstSets.computeFIRSTConcatenationRHS(elementsAfterNonterminalFirstOccurrence);
+        FIRST.forEach(element -> {
+            if (element.equals(Constants.EPSILON)) {
+                temporaryFollowSets.get(nonterminal).addAll(this.followSets.get(LHSNonterminal));
+            }
+            else {
+                temporaryFollowSets.get(nonterminal).addAll(this.firstSets.getPreviousFirst(element));
+            }
+        });
+    }
+
+    private void processNonterminal(Map<String, Set<String>> temporaryFollowSets, String nonterminal) {
+        temporaryFollowSets.put(nonterminal, new HashSet<>(this.followSets.get(nonterminal)));
+
+        List<Pair<String, Set<List<String>>>> productionsWhichContainNonterminal = this.findProductionsWhichContainNonterminal(nonterminal);
+        productionsWhichContainNonterminal.forEach(pair -> {
+            String LHSNonterminal = pair.getFirst();
+            Set<List<String>> productionRHS = pair.getSecond();
+            productionRHS.forEach(possibleProductionRHS -> this.processPossibleProductionRHS(temporaryFollowSets, nonterminal, possibleProductionRHS, LHSNonterminal));
+        });
+    }
+
+    private void initializeFollowSets() {
+        this.grammar.getNonterminals().forEach(nonterminal -> {
+            Set<String> newSet = new HashSet<>();
+            if (nonterminal.equals(this.grammar.getStartingSymbol())) {
+                newSet.add(Constants.END_OF_INPUT);
+            }
+            this.followSets.put(nonterminal, newSet);
+        });
+    }
+
+    private void computeFollowSets() {
+        this.followSets = new HashMap<>();
+        this.initializeFollowSets();
+
+        boolean sameSets = false;
+        while (! sameSets) {
+            sameSets = true;
+            Map<String, Set<String>> temporaryFollowSets = new HashMap<>();
+            for (String nonterminal : this.grammar.getNonterminals()) {
+                processNonterminal(temporaryFollowSets, nonterminal);
+                if (! temporaryFollowSets.get(nonterminal).equals(this.followSets.get(nonterminal))) {
+                    sameSets = false;
+                }
+            }
+            this.followSets = temporaryFollowSets;
+        }
+    }
+
+    public FollowSet(Grammar grammar, FirstSet firstSets) {
+        this.grammar = grammar;
+        this.firstSets = firstSets;
+        this.computeFollowSets();
+    }
+
+    public Map<String, Set<String>> getFollowSets() {
+        return this.followSets;
+    }
+}
--- src/Parser.java
+++ src/Parser.java
@@ -1,85 +1,14 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Parser {
-    private final Grammar grammar;
     private final FirstSet firstSets;
-    private Map<String, Set<String>> followSets;
-
-    private List<Pair<String, Set<List<String>>>> findProductionsWhichContainNonterminal(String nonterminal) {
-        List<Pair<String, Set<List<String>>>> productionsWhichContainNonterminal = new ArrayList<>();
-        this.grammar.getProductions().forEach((productionLHS, productionRHS) -> {
-            Set<List<String>> productionRHSWhichStartFromCurrentNonterminal = productionRHS
-                    .stream()
-                    .filter(possibleProductionRHS -> possibleProductionRHS.contains(nonterminal))
-                    .collect(Collectors.toSet());
-            if (! productionRHSWhichStartFromCurrentNonterminal.isEmpty()) {
-                productionsWhichContainNonterminal.add(new Pair<>(productionLHS.get(0), productionRHSWhichStartFromCurrentNonterminal));
-            }
-        });
-        return productionsWhichContainNonterminal;
-    }
-
-    private void processPossibleProductionRHS(Map<String, Set<String>> temporaryFollowSets, String nonterminal, List<String> possibleProductionRHS, String LHSNonterminal) {
-        List<String> elementsAfterNonterminalFirstOccurrence = possibleProductionRHS.subList(possibleProductionRHS.indexOf(nonterminal) + 1, possibleProductionRHS.size());
-        if (elementsAfterNonterminalFirstOccurrence.size() == 0) {
-            elementsAfterNonterminalFirstOccurrence.add(Constants.EPSILON);
-        }
-
-        Set<String> FIRST = this.firstSets.computeFIRSTConcatenationRHS(elementsAfterNonterminalFirstOccurrence);
-        FIRST.forEach(element -> {
-            if (element.equals(Constants.EPSILON)) {
-                temporaryFollowSets.get(nonterminal).addAll(this.followSets.get(LHSNonterminal));
-            }
-            else {
-                temporaryFollowSets.get(nonterminal).addAll(this.firstSets.getPreviousFirst(element));
-            }
-        });
-    }
-
-    private void processNonterminal(Map<String, Set<String>> temporaryFollowSets, String nonterminal) {
-        temporaryFollowSets.put(nonterminal, new HashSet<>(this.followSets.get(nonterminal)));
-
-        List<Pair<String, Set<List<String>>>> productionsWhichContainNonterminal = this.findProductionsWhichContainNonterminal(nonterminal);
-        productionsWhichContainNonterminal.forEach(pair -> {
-            String LHSNonterminal = pair.getFirst();
-            Set<List<String>> productionRHS = pair.getSecond();
-            productionRHS.forEach(possibleProductionRHS -> this.processPossibleProductionRHS(temporaryFollowSets, nonterminal, possibleProductionRHS, LHSNonterminal));
-        });
-    }
-
-    private void computeFollowSets() {
-        this.grammar.getNonterminals().forEach(nonterminal -> {
-            Set<String> newSet = new HashSet<>();
-            if (nonterminal.equals(this.grammar.getStartingSymbol())) {
-                newSet.add(Constants.END_OF_INPUT);
-            }
-            this.followSets.put(nonterminal, newSet);
-        });
-
-        boolean sameSets = false;
-        while (! sameSets) {
-            sameSets = true;
-            Map<String, Set<String>> temporaryFollowSets = new HashMap<>();
-            for (String nonterminal : this.grammar.getNonterminals()) {
-                processNonterminal(temporaryFollowSets, nonterminal);
-                if (! temporaryFollowSets.get(nonterminal).equals(this.followSets.get(nonterminal))) {
-                    sameSets = false;
-                }
-            }
-            this.followSets = temporaryFollowSets;
-        }
-    }
+    private final FollowSet followSets;
 
     public Parser(Grammar grammar) {
-        this.grammar = grammar;
-        this.firstSets = new FirstSet(this.grammar);
-        this.followSets = new HashMap<>();
-        this.computeFollowSets();
+        this.firstSets = new FirstSet(grammar);
+        this.followSets = new FollowSet(grammar, this.firstSets);
     }
 
     public void parse() {
         this.firstSets.getFirstSets().forEach((a, b) -> System.out.println(a + "---" + b));
-        this.followSets.forEach((a, b) -> System.out.println(a + "###" + b));
+        this.followSets.getFollowSets().forEach((a, b) -> System.out.println(a + "###" + b));
     }
 }
