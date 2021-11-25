dfcba7723f0603a1ae2fb56389ea551daada0065
GhermanCristian <gherman_cristi10@yahoo.com>
2021-11-25 14:01:35 +0200

    Added a method to determine all the productions which contain a given nonterminal on the RHS

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Parser {
     private final Grammar grammar;
@@ -7,8 +8,30 @@ public class Parser {
         this.grammar = grammar;
     }
 
+    private List<Pair<String, Set<List<String>>>> findProductionsWhichContainNonterminal(String nonterminal) {
+        List<Pair<String, Set<List<String>>>> productionsWhichContainNonterminal = new ArrayList<>();
+        this.grammar.getProductions().forEach((productionLHS, productionRHS) -> {
+            Set<List<String>> productionRHSWhichStartFromCurrentNonterminal = productionRHS
+                    .stream()
+                    .filter(possibleProductionRHS -> possibleProductionRHS.contains(nonterminal))
+                    .collect(Collectors.toSet());
+            if (! productionRHSWhichStartFromCurrentNonterminal.isEmpty()) {
+                productionsWhichContainNonterminal.add(new Pair<>(productionLHS.get(0), productionRHSWhichStartFromCurrentNonterminal));
+            }
+        });
+        return productionsWhichContainNonterminal;
+    }
+
     public void parse() {
         Map<String, Set<String>> firstSets = new FirstSet(this.grammar).getFirstSets();
         firstSets.forEach((a, b) -> System.out.println(a + "---" + b));
+        this.grammar.getNonterminals().forEach(nonterminal -> {
+            System.out.println("The following contain " + nonterminal);
+            this.findProductionsWhichContainNonterminal(nonterminal).forEach(pair -> {
+                System.out.print(pair.getFirst() + " -> ");
+                pair.getSecond().forEach(System.out::print);
+                System.out.println();
+            });
+        });
     }
 }
