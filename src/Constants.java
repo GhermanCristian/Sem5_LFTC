b09b657e5419959c30d10101454c7f3f4a5c9a3a
GhermanCristian <gherman_cristi10@yahoo.com>
2021-12-04 23:31:21 +0200

    Can now compute parse tables

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,6 @@
 public class Constants {
     public static final String EPSILON = "EPS";
     public static final String END_OF_INPUT = "$";
+    public static final String POP = "POP";
+    public static final String ACCEPTED = "ACC";
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,14 +1,50 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Parser {
     private final FirstSet firstSets;
     private final FollowSet followSets;
+    private final Grammar grammar;
+    private final Map<String, Map<String, Pair<List<String>, Integer>>> parseTable; // row -> column -> (productionRHS, number)
 
     public Parser(Grammar grammar) {
         this.firstSets = new FirstSet(grammar);
         this.followSets = new FollowSet(grammar, this.firstSets);
+        this.grammar = grammar;
+        this.parseTable = this.generateParseTable();
+    }
+    
+    private Map<String, Map<String, Pair<List<String>, Integer>>> generateParseTable() {
+        Map<String, Map<String, Pair<List<String>, Integer>>> table = new HashMap<>();
+        this.grammar.getProductions().forEach((LHS, productionSet) ->
+            productionSet.forEach(productionRHS -> {
+                table.putIfAbsent(LHS.get(0), new HashMap<>());
+                //TODO - use actual production numbers
+                if (productionRHS.size() == 1 && productionRHS.get(0).equals(Constants.EPSILON)) {
+                    this.followSets.getFollowSets().get(LHS.get(0))
+                            .forEach(element -> table.get(LHS.get(0)).put(element, new Pair<>(List.of(Constants.EPSILON), 0)));
+                }
+                else {
+                    this.firstSets.computeFIRSTConcatenationRHS(productionRHS).forEach(element -> {
+                        productionRHS.remove(Constants.EPSILON);
+                        table.get(LHS.get(0)).put(element, new Pair<>(productionRHS, 0));
+                    });
+                }
+            }));
+
+        this.grammar.getTerminals().forEach(terminal -> {
+            table.putIfAbsent(terminal, new HashMap<>());
+            table.get(terminal).put(terminal, new Pair<>(List.of(Constants.POP), 0));
+        });
+        table.putIfAbsent(Constants.END_OF_INPUT, Map.of(Constants.END_OF_INPUT, new Pair<>(List.of(Constants.ACCEPTED), 0)));
+
+        return table;
     }
 
     public void parse() {
         this.firstSets.getFirstSets().forEach((a, b) -> System.out.println(a + "---" + b));
         this.followSets.getFollowSets().forEach((a, b) -> System.out.println(a + "###" + b));
+        this.parseTable.forEach((a, b) -> b.forEach((c, d) -> System.out.print("[" + a + "][" + c + "] = " + d + "\n")));
     }
 }
