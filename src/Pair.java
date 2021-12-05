dd2b79b8656f0a03257f28fdf7557e6c0e4aa828
GhermanCristian <gherman_cristi10@yahoo.com>
2021-12-05 15:21:59 +0200

    Added equals() and hashCode() to Pair

--- src/Pair.java
+++ src/Pair.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Pair<F, S> {
     private final F first;
     private final S second;
@@ -18,5 +20,18 @@ public class Pair<F, S> {
     @Override
     public String toString() {
         return "(" + this.first.toString() + ", " + this.second.toString() + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return first.equals(pair.first) && second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
     }
 }
